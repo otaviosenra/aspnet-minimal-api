a543db93405d1e77d6b9d48417bc30f22dbd09d8
Otavio <araujofletes@gmail.com>
2025-07-24 13:36:42 -0300

    ajuste dto login

--- Domain/DTOs/LoginDTO.cs
+++ Domain/DTOs/LoginDTO.cs
@@ -1,7 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace MinimalApi.Domain.DTOs;
 
     public class LoginDTO
     {
+        [Required]
         public string Username { get; set; } = default!;
+
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        [MinLength(3)]
         public string Password { get; set; } = default!;
     }
